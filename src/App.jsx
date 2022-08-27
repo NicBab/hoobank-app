cd978ab2556f2985eba159d8b2e4f4d7adecf758
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-08-26 22:54:21 -0500

    components layout & add style.js folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import { useState } from 'react'
+import React from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
 
-function App() {
- 
+const App = () => {
   return (
-    <div className="App">
-Hello
-    </div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
 
-export default App
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Billing />
+          <CardDeal />
+          <Business />
+          <Clients />
+          <CTA />
+          <Stats />
+          <Footer />
+          <Testimonials />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
